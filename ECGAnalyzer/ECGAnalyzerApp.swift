f6819a5566239974f9cd4f3a0de8a78f6890e699
Adelenarci <kaan.karagozgil@icloud.com>
2025-05-08 12:10:47 +0300

    Initial Commit

---
+++ ECGAnalyzer/ECGAnalyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ECGAnalyzerApp.swift
+//  ECGAnalyzer
+//
+//  Created by Ali Kaan Karagözgil on 8.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct ECGAnalyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
